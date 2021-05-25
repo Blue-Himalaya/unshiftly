04457645f4efeac2c676202234be53442e72107a
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-05-25 15:08:01 -0400

    Load shifts from subcomponent and add buttons at the bottom

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,36 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import ActivityList from './ActivityList.jsx';
+import Shifts from './shifts.jsx';
 
 const Calendar = () => {
-  const schedule = useSelector(state => state.scheduleReducer.schedule);
-
-  console.log(schedule)
 
   return (
-    <div className="view-container">
-      <div className="cal-container">
-        <h1>Calendar</h1>
-        <div className="cal-contents-cont">
-            <div id="calendar-dates">
-              <div>Fri</div>
-              <div>Sat</div>
-              <div>Sun</div>
-              <div>Mon</div>
-              <div>Tue</div>
-              <div>Wed</div>
-              <div>Thu</div>
+    <div>
+      <div className="view-container">
+        <div className="cal-container">
+          <h1>Calendar</h1>
+          <div className="cal-contents-cont">
+              <div id="columns">
+                <div>Fri</div>
+                <div>Sat</div>
+                <div>Sun</div>
+                <div>Mon</div>
+                <div>Tue</div>
+                <div>Wed</div>
+                <div>Thu</div>
+              </div>
+              <Shifts />
             </div>
-            <div>shifts</div>
-          </div>
-      </div>
-
-        <div className="activity-log-container">
-          <div className="activity-grid">
-            <ActivityList />
-          </div>
         </div>
 
+          <div className="activity-log-container">
+            <div className="activity-grid">
+              <ActivityList />
+            </div>
+          </div>
+      </div>
+      <button type="click">Edit Calendar</button>
+      <button type="click">Edit Employees</button>
     </div>
   );
 }
