100e141d7c3655d367018997fd2921b7bf14fdca
Ryan Pannone <rpannnone88@gmail.com>
2021-05-25 17:27:19 -0400

    implement requestSingleDayOff

--- database/index.js
+++ database/index.js
@@ -7,7 +7,8 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
-  port: '3306'
+  port: '3306',
+  multipleStatements: true
 });
 
 module.exports = connection;
