d2d33c33f6151f0bbef792d9be90e81bdab420a7
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-05-25 23:25:28 -0400

    Make list of days. Dynamically render shifts through subcomponent

--- src/components/Calendar/shifts.jsx
+++ src/components/Calendar/shifts.jsx
@@ -1,70 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import DayOfWeek from './dayOfWeek.jsx';
 import moment from 'moment';
 
 const Shifts = () => {
-  const schedule = useSelector(state => state.scheduleReducer.schedule[0]);
-  console.log(schedule)
+  const schedule = useSelector(state => state.scheduleReducer.schedule);
+  console.log('schedule: ', schedule)
 
-  // Get shifts into an object of arrays
-  // let getShifts = () => {
-  //   var allShifts = {
-  //     'Friday': [],
-  //     'Saturday': [],
-  //     'Sunday': [],
-  //     'Monday': [],
-  //     'Tuesday': [],
-  //     'Wednesday': [],
-  //     'Thursday': []
-  //   };
-  //   for (var day in schedule) {
-  //     var shifts = schedule[day];
-  //     shifts.forEach((shift) => {
-  //       allShifts[day].push([shift.datetime, shift.name, shift.role])
-  //     })
-  //   }
-  //   return allShifts;
-  // }
-  // let shifts = getShifts();
-  // // console.log(shifts)
+  var days = ['Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday']
 
-  const handleClick = (e) => {
-    // e.stopPropagation();
-    console.log(e.target);
-    console.log(e.target.value)
-  }
+  // october 2019 11th - 18th
+  // collection of days
+  // create components for each day of week
+    // for this day component pass it the list of shifts for that day
+    // loop through shifts
+    // dynamically render
 
 
   return(
-    <div id="columns">
-      <div>
-        {schedule.Friday.map((shift) => (
-          <div onClick={(e) => handleClick(e)} value={shift.phone}>
-            {shift.datetime} <br/>
-            {shift.name} <br />
+      <div className="column-container">
+      {days.map((day) => (
+          <div className="column">
+            <DayOfWeek shifts={schedule[0].[day]} />
           </div>
-        ))}
+      ))}
       </div>
-      <div>
-        {/* {shifts.Saturday.map(shift =>
-          <div>{shift}</div>)} */}
-      </div>
-      <div>
-        {/* {shifts.Sunday} */}
-      </div>
-      <div>
-        {/* {shifts.Monday} */}
-      </div>
-      <div>
-        {/* {shifts.Tuesday} */}
-      </div>
-      <div>
-        {/* {shifts.Wednesday} */}
-      </div>
-      <div>
-        {/* {shifts.Thursday} */}
-      </div>
-    </div>
   )
 }
 
