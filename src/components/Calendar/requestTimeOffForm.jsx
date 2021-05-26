3e973e593d0e904ff54d52b423f639392da8872f
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-05-26 16:47:47 -0400

    Implement request time off form

---
+++ src/components/Calendar/requestTimeOffForm.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+const RequestTimeOffForm = (props) => {
+  const user = useSelector(state => state.viewReducer.user)
+  const { isOpen } = props;
+  console.log(user);
+  return (
+    <div>
+      {isOpen
+      ? <div className="emp-info-modal-bg">
+          <div className="emp-info-modal">
+            Request Time Off
+            <br/>
+            Date: <input className="date-picker" type="date"></input>
+            <br/>
+            Split: <select>
+              <option value="">Pick</option>
+              <option value="1">AM</option>
+              <option value="0">PM</option>
+            </select>
+            Employee info: {}
+            <button type="submit">Submit request</button>
+          </div>
+      </div>
+      : null}
+      {/* Request time off form */}
+    </div>
+  )
+}
+
+export default RequestTimeOffForm;
