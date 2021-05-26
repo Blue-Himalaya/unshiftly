4bd34774619d3c5fddadf0a6b6e9b0898ae275e5
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-05-26 16:29:34 -0400

    Make reducer for single time request

--- redux-state/reducers/timeOffReducer.js
+++ redux-state/reducers/timeOffReducer.js
@@ -1,8 +1,19 @@
+const initialState = {
+  timeOff: [],
+  reqTimeOff: [],
+}
 const timeOffReducer = (state = [], action) => {
   switch (action.type) {
     case 'GET_TIMEOFF': {
       return {
+        ...state,
         timeOff: action.payload,
+      }
+    }
+    case 'REQ_TIMEOFF': {
+      return {
+        ...state,
+        reqTimeOff: action.payload,
       }
     }
     default:
