1dff766f41ff4285e649148aee9d3813434e7ee1
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-05-27 17:33:21 -0400

    WIP. Render the proper data from parent compoenent

--- src/components/Calendar/employeeInfoModal.jsx
+++ src/components/Calendar/employeeInfoModal.jsx
@@ -3,24 +3,30 @@ import { useSelector } from 'react-redux';
 
 const EmployeeInfoModal = (props) => {
   let employeeInfo = props.info;
-  // console.log(employeeInfo)
   let isOpen = props.isOpen;
 
+  const user = useSelector(state => state.viewReducer.user)
+
+  const { openModal } = props;
   // if the modal is not open return null
   // else the code below (modal) will render
+
+  // put a check for user matching the name of the person in shift block
+
   return (
-    <div>
+    <div className="shift-block-cont">
       {isOpen?
         <div className="emp-info-modal-bg-2 modal-outer">
           <div className="emp-info-modal-2 modal-inner">
-            Employee: {employeeInfo.name}
+            Employee: {employeeInfo[0]}
             <br/>
-            Phone: {employeeInfo.phone}
+            Phone: {employeeInfo[1]}
+            {user === employeeInfo[0]
+            ? <button type="click" onClick={() => console.log('release shift sent')}>Submit</button>
+            : null}
           </div>
         </div>
-        : null
-      }
-
+        : null}
     </div>
   )
 }
