effc4f7e4f0da4c288996ace9d844c6292f49bd9
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-05-27 17:31:38 -0400

    Delete release shift button

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Shifts from './shifts.jsx';
 import RequestTimeOffForm from './requestTimeOffForm.jsx';
+import ReleaseShiftForm from './releaseShiftForm.jsx';
 import ActivityList from './ActivityList/ActivityList.jsx';
 
 const Calendar = () => {
@@ -12,42 +13,16 @@ const Calendar = () => {
   const columnDates = useSelector(state => state.scheduleReducer.listOfDays);
   const currentDateInfo = useSelector(state => state.scheduleReducer.currentDate).split('-');
 
-  //console.log(columnDates);
-  //console.log(currentDateInfo);
   let today = `${currentDateInfo[1]} ${currentDateInfo[2]} ${currentDateInfo[0]}`
 
   const [isFormOpen, setToggleForm] = useState(false);
-
-  // render days (num) dynamically
-
-  // check if admin is true for admin btns + functionality
-  // if admin is false
-  // render the "request time off (RTO)" btns & "release shift"
-
-  /*
-  === REQUEST TIME OFF ===
-
-  Employees can only click on their shifts only
-  -When clicked a modal with info & btns pops
-  -Has shift info
-  -Has "RTO" btn & release schedule btn
-
-  RTO button needs the shift id of the clicked shift
-
-  2- click a "RTO" btn
-     modal pops up with field inputs
-      -Choose the date (Request a (single) whole day off)
-      -Choose whether it's "one time" or "recurring" time off
-      -If admin approves the request
-      -The calendar should reflect for both the admin
-       and the employee who requested greyed out blocks
-  */
-  // console.log('roles: ', roles);
-  // console.log('timeoff: ', timeOff);
-  // console.log('employees: ', employees);
+  const [isReleaseFormOpen, setToggleReleaseForm] = useState(false);
 
   const timeOffReqForm = () => {
     setToggleForm(prevIsFormOpen => !prevIsFormOpen)
+  }
+  const toggleReleaseShiftForm = () => {
+    setToggleReleaseForm(prevIsReleaseFormOpen => !prevIsReleaseFormOpen)
   }
 
   return (
@@ -83,8 +58,9 @@ const Calendar = () => {
           </div>
       </div>
       <button type="click" onClick={timeOffReqForm}>Request Time Off</button>
-      <RequestTimeOffForm isOpen={isFormOpen} closeModal={timeOffReqForm}/>
-      <button type="click">Release Shift</button>
+      <RequestTimeOffForm isOpen={isFormOpen} closeModal={timeOffReqForm} />
+      {/* <button type="click"  onClick={toggleReleaseShiftForm}>Release Shift</button>
+      <ReleaseShiftForm isOpen={isReleaseFormOpen} closeModal={toggleReleaseShiftForm} /> */}
     </div>
   );
 }
