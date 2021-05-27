cf7db0c43fb49dea1a732d93944fa1345fca46a0
Jim Carter <jameslesliecarter@gmail.com>
2021-05-27 11:09:15 -0400

    adding terraform and appropriate gitignore

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami = "ami-0d5eff06f840b45e9"
+  instance_type = "t2.micro"
+  key_name = "knowledge_key"
+  security_groups = ["unshiftly-web"]
+  tags = {
+    "Name" = "Unshiftly-Web-App"
+  }
+  ebs_block_device {
+    device_name = "/dev/xvda"
+    volume_type = "gp2"
+    volume_size = "25"
+  }
+}
