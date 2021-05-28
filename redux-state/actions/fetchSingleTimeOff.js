a8efa1716e0dbc94f37bd2988402e0942e644e36
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-05-27 21:29:12 -0400

    Take in an offset and use it

--- redux-state/actions/fetchSingleTimeOff.js
+++ redux-state/actions/fetchSingleTimeOff.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
-export const fetchSingleTimeOff = (date) => {
+export const fetchSingleTimeOff = (date, offset = 0) => {
+
+  console.log(date)
 
   const days = ['Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday']
   const daysOrdered = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
@@ -10,8 +12,8 @@ export const fetchSingleTimeOff = (date) => {
   //GET POSITION OF DAY FROM SCHEDULE WEEK FRI-THU
   var index = days.indexOf(daysOrdered[startDate.getUTCDay()])
 
-  startDate.setDate(startDate.getDate() - index);
-  endDate.setDate(endDate.getDate() + 7 - index - 1)
+  startDate.setDate(startDate.getDate() - index + offset);
+  endDate.setDate(endDate.getDate() + 7 - index - 1 + offset)
 
   return (dispatch) => {
     axios.get('/allSingleTimeOff', {
