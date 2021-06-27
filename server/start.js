8559edf2d1a5e9d2bb8fd898ccf2ea923a2524d9
Jim Carter <jameslesliecarter@gmail.com>
2021-06-27 13:23:49 -0400

    still allowing for local dev without manipulation

--- server/start.js
+++ server/start.js
@@ -3,9 +3,13 @@ const https = require('https');
 const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config()
-// app.listen(8080, () => {
-//   console.log('server listening on port 8080');
-// });
+// local dev server config
+app.listen(8080, () => {
+  console.log('server listening on port 8080');
+});
+
+// deployed prod server config
+/*
 const options = {
   key: fs.readFileSync(process.env.SSL_KEY, 'utf8'),
   cert: fs.readFileSync(process.env.SSL_CERT, 'utf8')
@@ -14,4 +18,4 @@ const options = {
 https.createServer(options, app).listen(8080, () => {
   console.log('HTTPS Server running on port 8080');
 });
-
+*/
