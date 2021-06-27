e593b6d66e75defe03f678689f430fc66a76eda6
Jim Carter <jameslesliecarter@gmail.com>
2021-06-27 12:44:43 -0400

    adding ssl

--- database/index.js
+++ database/index.js
@@ -2,6 +2,18 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
+// Local Dev Config
+const connection = mysql.createConnection({
+  host: process.env.LOCAL_DB_HOST,
+  user: process.env.LOCAL_DB_USER,
+  password: process.env.LOCAL_DB_PASS,
+  database: process.env.LOCAL_DB_DATABASE,
+  port: '3306',
+  multipleStatements: true
+});
+
+// Standard Deploy Config
+/*
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -10,5 +22,19 @@ const connection = mysql.createConnection({
   port: '3306',
   multipleStatements: true
 });
+*/
+
+// Docker config
+/*
+const connection = mysql.createConnection({
+  host: process.env.DOCKER_PROD_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  port: '3306',
+  multipleStatements: true
+});
+*/
+
 
 module.exports = connection;
--- server/start.js
+++ server/start.js
@@ -1,5 +1,17 @@
 const app = require('./index.js');
-app.listen(8080, () => {
-  console.log('server listening on port 8080');
+const https = require('https');
+const fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config()
+// app.listen(8080, () => {
+//   console.log('server listening on port 8080');
+// });
+const options = {
+  key: fs.readFileSync(process.env.SSL_KEY, 'utf8'),
+  cert: fs.readFileSync(process.env.SSL_CERT, 'utf8')
+};
+
+https.createServer(options, app).listen(8080, () => {
+  console.log('HTTPS Server running on port 8080');
 });
 
